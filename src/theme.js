b4203322e66f42426faca4fce99d3609f16f064f
Sang <sang05112003@gmail.com>
2023-10-12 23:34:19 +0700

    App bar

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,14 @@
-import { createTheme } from '@mui/material/styles'
+
 import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
-import { cyan, deepOrange, orange, red, teal } from '@mui/material/colors'
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
     trelloCustom: {
-        appBarHeight: '48px',
-        boardBarHeight: '58px'
+        appBarHeight: '58px',
+        boardBarHeight: '60px'
     },
+
     colorSchemes: {
         light: {
             palette: {
@@ -22,6 +23,47 @@ const theme = extendTheme({
             },
         },
     },
+
+    components: {
+        MuiButton: {
+          styleOverrides: {
+            root: {
+              textTransform: 'none'
+            },
+          },
+        },
+
+        MuiInputLabel: {
+            styleOverrides: {
+              root: ({theme}) => ({
+                color: theme.palette.primary.main,
+                fontSize: '0.875rem'
+              })
+            },
+        },
+
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: ({theme}) => {
+                    return {
+                        color: theme.palette.primary.main,
+                        fontSize: '0.875rem',
+                        'MuiOutlinedInput-notchedOutline':{
+                            borderColor: theme.palette.primary.light
+                        },
+                        '&:hover': {
+                            'MuiOutlinedInput-notchedOutline':{
+                                borderColor: theme.palette.primary.main
+                            },
+                        },
+                        '& fieldset': {
+                            borderWidth: '1px !important'
+                        }
+                    }
+                }
+            }
+          }
+      }
 })
 
 export default theme
