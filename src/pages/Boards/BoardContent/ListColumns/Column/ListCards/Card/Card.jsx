3e98696576df4a65a035c593d76ceefcb602246b
S52100102 <52100102@student.tdtu.edu.vn>
2023-10-19 01:53:10 +0700

    Refactor structure boardContent

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,50 @@
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import GroupsIcon from '@mui/icons-material/Groups'
+import CommentIcon from '@mui/icons-material/Comment'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+
+function Card( {temporaryHideMedia} ){
+    if(temporaryHideMedia){
+        return(
+            <MuiCard sx={{ 
+                cursor: 'pointer',
+                boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+                overflow: 'unset'
+            }}>
+               
+                <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+                    <Typography>Panda</Typography>
+                </CardContent>
+               
+            </MuiCard>
+        )
+    }
+    return (
+        <MuiCard sx={{ 
+            cursor: 'pointer',
+            boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+            overflow: 'unset'
+        }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8TT54hJZO28765CFATTfuqrZDmn3gDDW1e90E3GNP3wXjeBIFs5OuX2rk7x56N9JfahU&usqp=CAU"
+                title="green iguana"
+            />
+            <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+                <Typography>Panda</Typography>
+            </CardContent>
+            <CardActions sx={{ p: '0 4px 8px 4px' }}>
+                <Button size="small" startIcon={<GroupsIcon/>}>20</Button>
+                <Button size="small" startIcon={<CommentIcon/>}>20</Button>
+                <Button size="small" startIcon={<AttachmentIcon/>}>20</Button>
+            </CardActions>
+        </MuiCard>
+    )
+}
+
+export default Card
